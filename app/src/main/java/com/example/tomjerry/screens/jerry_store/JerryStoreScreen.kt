b79616824aba91a715b4f66dd25f16090f2c8606
dina othman <dinaosman581@gmail.com>
2025-05-29 16:28:19 +0300

    add jerry store Search

--- app/src/main/java/com/example/tomjerry/screens/jerry_store/JerryStoreScreen.kt
+++ app/src/main/java/com/example/tomjerry/screens/jerry_store/JerryStoreScreen.kt
@@ -1,1 +1,35 @@
 package com.example.tomjerry.screens.jerry_store
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tomjerry.screens.jerry_store.components.Header
+import com.example.tomjerry.screens.jerry_store.components.Search
+
+@Composable
+fun JerryStoreScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFEEF4F6))
+            .padding(horizontal = 16.dp)
+    ) {
+        Header()
+        Spacer(modifier = Modifier.height(12.dp))
+        Search()
+    }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun JerryStoreScreenPreview(){
+    JerryStoreScreen()
+}
---
+++ app/src/main/java/com/example/tomjerry/screens/jerry_store/components/Search.kt
@@ -0,0 +1,90 @@
+package com.example.tomjerry.screens.jerry_store.components
+
+import androidx.compose.foundation.background
+import com.example.tomjerry.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tomjerry.ui.theme.IbmPlexSans
+
+
+
+@Composable
+fun Search(){
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        OutlinedTextField(
+            value = "" ,
+            onValueChange = {} ,
+            modifier = Modifier.height(48.dp),
+            placeholder = {
+                Text(
+                    text = "Search about tom ...",
+                    style = TextStyle(
+                        color = Color(0xFF969799) ,
+                        fontFamily = IbmPlexSans ,
+                        fontWeight = FontWeight.Normal ,
+                        fontSize = 14.sp
+                    )
+                )
+            } ,
+            leadingIcon = {
+                Icon(
+                    painterResource(R.drawable.ic_search) ,
+                    contentDescription = "search icon" ,
+                    tint = Color(0xFF969799),
+                    modifier = Modifier.size(24.dp)
+                )
+            } ,
+            shape = MaterialTheme.shapes.small ,
+            colors =  OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Color.White, focusedContainerColor = Color.White,
+                unfocusedBorderColor = MaterialTheme.colorScheme.outlineVariant.copy(alpha = 0.08f),
+                focusedBorderColor = MaterialTheme.colorScheme.outlineVariant.copy(alpha = 0.08f),
+            )
+        )
+
+        IconButton(
+            onClick = { },
+            modifier = Modifier.size(48.dp)
+                .background(Color(0xFF03578A), MaterialTheme.shapes.small)
+        ) {
+            Icon(
+                modifier = Modifier.padding(12.dp),
+                painter = painterResource(id = R.drawable.ic_filter),
+                contentDescription = "filter icon",
+                tint = Color.White,
+            )
+        }
+    }
+
+    }
+
+
+@Composable
+@Preview(showSystemUi = true , showBackground = true)
+fun SearchPreview(){
+    Search()
+}
