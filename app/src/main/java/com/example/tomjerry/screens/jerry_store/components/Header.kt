beb217a78324181eb84b270cbd44bb7c70915f18
dina othman <dinaosman581@gmail.com>
2025-05-29 15:09:58 +0300

    add jerry store header

--- app/src/main/java/com/example/tomjerry/screens/jerry_store/components/Header.kt
+++ app/src/main/java/com/example/tomjerry/screens/jerry_store/components/Header.kt
@@ -1,2 +1,106 @@
 package com.example.tomjerry.screens.jerry_store.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tomjerry.R
+import com.example.tomjerry.ui.theme.IbmPlexSans
+import androidx.compose.material3.Icon
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+
+
+@Composable
+fun Header(){
+    Row(
+        modifier = Modifier
+            .padding(top = 4.dp)
+            .fillMaxWidth() ,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(R.drawable.profile_image) ,
+            contentDescription = "profile image" ,
+            contentScale = ContentScale.Crop ,
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.small)
+                .size(48.dp)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Text(
+                text = "Hi, Jerry \uD83D\uDC4B\uD83C\uDFFB" ,
+                color = Color(0xFF1F1F1E),
+                fontFamily = IbmPlexSans ,
+                fontWeight = FontWeight.Medium ,
+                fontSize = 14.sp
+            )
+            Text(
+                text = "Which Tom do you want to buy?" ,
+                color = Color(0xFFA5A6A4),
+                fontFamily = IbmPlexSans ,
+                fontWeight = FontWeight.Normal ,
+                fontSize = 12.sp
+            )
+        }
+
+        BadgedBox(
+            modifier = Modifier.padding(end = 4.dp),
+            badge = {
+                Badge(containerColor = Color(0xFF03578A)) {
+                    Text("3", color = Color.White, fontSize = 10.sp)
+                }
+            }
+        ) {
+            Box(
+                modifier = Modifier
+                    .border(
+                        width = 1.dp,
+                        color = LightGray,
+                        shape = MaterialTheme.shapes.small
+                    )
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_notifications),
+                    contentDescription = "notification icon",
+                )
+            }
+        }
+
+
+
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun HeaderPreview(){
+    Header()
+}
--- app/src/main/java/com/example/tomjerry/ui/theme/Type.kt
+++ app/src/main/java/com/example/tomjerry/ui/theme/Type.kt
@@ -2,11 +2,12 @@ package com.example.tomjerry.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.tomjerry.R
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -15,20 +16,12 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+val IbmPlexSans = FontFamily(
+    Font(R.font.ibm_plex_sans_arabic_regular , weight = FontWeight.Normal) ,
+    Font(R.font.ibm_plex_sans_arabic_medium , weight = FontWeight.Medium) ,
+    Font(R.font.ibm_plex_sans_arabic_bold , weight = FontWeight.Bold) ,
+    Font(R.font.ibm_plex_sans_arabic_semibold , weight = FontWeight.SemiBold) ,
+    Font(R.font.ibm_plex_sans_arabic_thin , weight = FontWeight.Light)
 )
