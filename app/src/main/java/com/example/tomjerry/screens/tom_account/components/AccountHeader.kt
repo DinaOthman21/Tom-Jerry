2bd245d636e25592962d8f2a7f242b974bbd7554
dina othman <dinaosman581@gmail.com>
2025-06-02 12:53:20 +0300

    edit AccountHeader

--- app/src/main/java/com/example/tomjerry/screens/tom_account/components/AccountHeader.kt
+++ app/src/main/java/com/example/tomjerry/screens/tom_account/components/AccountHeader.kt
@@ -1,14 +1,17 @@
 package com.example.tomjerry.screens.tom_account.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,48 +32,68 @@ import com.example.tomjerry.ui.theme.IbmPlexSans
 
 @Composable
 fun AccountHeader(modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
         modifier = modifier
             .fillMaxWidth()
-            .background(Color.Blue)
-            .padding(horizontal = 16.dp)
-            .padding(top = 16.dp, bottom = 8.dp)
+            .height(200.dp)
     ) {
         Image(
-            painter = painterResource(id = R.drawable.tom),
-            contentDescription = "account image",
+            painter = painterResource(id = R.drawable.background_container),
+            contentDescription = "background image",
             contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
+        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .size(64.dp)
-                .clip(MaterialTheme.shapes.small)
-        )
-        Spacer(Modifier.height(4.dp))
-        Text(
-            "Tom",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Medium,
-            fontFamily = IbmPlexSans,
-            color = Color.White,
-        )
-        Text(
-            "specializes in failure!",
-            fontSize = 12.sp,
-            fontWeight = FontWeight.Normal,
-            fontFamily = IbmPlexSans,
-            color = Color.White,
-        )
-        Spacer(Modifier.height(4.dp))
-        Text(
-            modifier = Modifier
-                .padding(horizontal = 12.dp, vertical = 6.dp),
-            text = "Edit foolishness",
-            fontSize = 10.sp,
-            fontWeight = FontWeight.Medium,
-            fontFamily = IbmPlexSans,
-            color = Color.White,
-        )
+                .padding(horizontal = 124.dp)
+                .align(Alignment.Center)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.tom),
+                contentDescription = "account image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(MaterialTheme.shapes.small)
+            )
+            Spacer(Modifier.height(4.dp))
+                Text(
+                    "Tom",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium,
+                    fontFamily = IbmPlexSans,
+                    color = Color.White,
+                )
+                Text(
+                    "specializes in failure!",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = IbmPlexSans,
+                    color = Color.White.copy(alpha = .8f) ,
+                    modifier = Modifier.fillMaxWidth() ,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(Modifier.height(4.dp))
+                Button(
+                    onClick = { },
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White.copy(alpha = 0.12f),
+                        contentColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(40.dp)
+                ) {
+                    Text(
+                        text = "Edit foolishness",
+                        fontSize = 10.sp,
+                        fontWeight = FontWeight.Medium,
+                        fontFamily = IbmPlexSans
+                    )
+                }
+        }
     }
 }
 
